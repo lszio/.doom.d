88332b86a1d6174ce04b6961d9ca2b28045ad9d3
Liszt21 <1832666492@qq.com>
2020-11-13 09:27:14 +0800

    enbale garbage collection in windows

--- config.el
+++ config.el
@@ -111,3 +111,10 @@
 (use-package leetcode)
 
 (setq global-wakatime-mode t)
+
+(when (eq system-type 'windows-nt)
+  (setq gc-cons-threshold (* 512 1024 1024))
+  (setq gc-cons-percentage 0.5)
+  (run-with-idle-timer 5 t #'garbage-collect)
+  ;; 显示垃圾回收信息，这个可以作为调试用 ;;
+  (setq garbage-collection-messages t) )
